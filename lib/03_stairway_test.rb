2e35588d04aa3730cec5dd7667fca3a57b82838f
Emilie-thp <emilie.marjollet@orange.fr>
2019-10-03 19:04:45 +0200

    jeu

---
+++ lib/03_stairway_test.rb
@@ -0,0 +1,79 @@
+def stairway
+	puts "Welcome to the Stairway game ! Throw the dice and try to climb the stairs"
+	puts "Press ENTER to play and throw the dice"
+	position=0
+	counter=0 
+
+	while position <10
+
+		a=rand(1..6)
+		puts "Dice=#{a}"
+
+
+		if a==5 || a==6 
+			puts "Congrats, you go to the next stair"
+			position = position+1
+			puts "You're now on the stair n°#{position}"
+			half_pyramid(position)
+			puts "Press ENTER again"
+			counter=counter+1
+		
+		end
+
+		if a==1   
+			puts "Sorry, you go down"
+				if position >0 then position = position-1
+				else position=position 
+				end
+			puts "You're now on the stair n°#{position}"
+			half_pyramid(position)
+			puts "Press ENTER again"
+			counter=counter+1
+
+		end
+
+		if a==2 || a==3 || a==4 
+			puts "sorry, you stay here"
+			position=position
+			puts "you're still on the stair n°#{position}"
+			half_pyramid(position)
+			puts "Press ENTER again"
+			counter=counter+1
+
+		end
+
+	end
+
+	puts "YOU WIN! You did #{counter} tours"
+	return counter
+	
+end
+
+
+def half_pyramid(chiffre)
+	for i in 1..chiffre
+	(chiffre-i).times {print " "} 
+	i.times {print "#"}
+	print "\n"
+	i=i+1
+	end
+end
+
+
+def average_finish_time(variable)
+	x=0
+	10.times do stairway 
+	x = (x+variable)
+	end
+puts x
+
+end
+
+def perform 
+counter=stairway
+average_finish_time(counter) 
+puts average_finish_time()
+
+end
+
+perform
