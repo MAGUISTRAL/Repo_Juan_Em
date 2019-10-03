8a449e5bce9f0cc89df924426db0fe7dbfed8e91
juanlfr <fuentes0091@gmail.com>
2019-10-03 11:35:43 +0200

    add exo 01_pyramids.rb

---
+++ lib/01_pyramids.rb
@@ -0,0 +1,22 @@
+
+def half_pyramid (chiffre)
+    n = 1
+        for i in 1..chiffre 
+        print " " * (chiffre- i)
+        puts  "#" * n 
+        n += 2
+        end
+    
+end
+
+puts "Choisis un chiffre"
+chiffre=gets.chomp.to_i
+while chiffre < 1 || chiffre>25
+puts "Choisis un chiffre entre 1 et 25"
+chiffre=gets.chomp.to_i
+end
+puts half_pyramid (chiffre)
+
+
+
+
